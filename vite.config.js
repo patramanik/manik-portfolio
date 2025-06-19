870151880d5c36e909b3675fb95ab26e53cb725a
kaushik <kaushikpaul29999@gmail.com>
2025-06-19 22:12:33 +0530

    build add

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // this is my vite modify for run index.html
   plugins: [
     tailwindcss(),
     react(),
   ],
-})
+});
